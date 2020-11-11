fedf875dee3f47ebf5dc9350bfb3fd11e9a26e4f
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-11 17:27:25 -0300

    testando operadores aritméticos

---
+++ aula07A.py
@@ -0,0 +1,11 @@
+'Soma'
+n1 = int(input('Qual valor?'))
+n2 = int(input('Qual valor?'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+dint = n1 // n2
+pot = n1 ** n2
+print("A soma é {}. o produtoo é {} e a divisão é {:.3f}".format(s, m, d))
+print("Divisão inteira é {} e potência é {}".format(dint,pot))
+
