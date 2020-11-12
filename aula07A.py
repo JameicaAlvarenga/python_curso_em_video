715a6a68a75c743136b946b8fdbc080669c624f8
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-12 16:12:45 -0300

    testando operadores aritméticos

--- aula07A.py
+++ aula07A.py
@@ -6,6 +6,6 @@ m = n1 * n2
 d = n1 / n2
 dint = n1 // n2
 pot = n1 ** n2
-print("A soma é {}. o produtoo é {} e a divisão é {:.3f}".format(s, m, d))
+print("A soma é {}. o produtoo é {} e a divisão é {:.3f}".format(s, m, d), end=' ')
 print("Divisão inteira é {} e potência é {}".format(dint,pot))
 
