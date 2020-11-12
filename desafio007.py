d1100ff7ca7c11351835c2609f57ed15ae375178
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-12 16:32:42 -0300

    média de duas notas

---
+++ desafio007.py
@@ -0,0 +1,5 @@
+"Desenvolva um programa que leia as duas notas do aluno e calcule a média"
+
+n1 = float(input('Informe a primeira nota: '))
+n2 = float(input('Informe a segunda nota: '))
+print("A méida do aluno é :{}". format((n1+n2)/2))
