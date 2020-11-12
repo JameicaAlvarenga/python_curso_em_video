f08e88d566660f36c4c271d04e161a125708688f
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-12 16:39:42 -0300

    "Converter metro em milimetro e centimetro"

---
+++ desafio008.py
@@ -0,0 +1,7 @@
+"Converter metro em milimetro e centimetro"
+
+m = float(input('Digite a metragem desejada:'))
+c = m * 100
+mil = m * 1000
+print('Valor em centimetros é: {}'.format(c))
+print('Valor em milimetros é:{}'.format(mil))
