472084513415c8c232b3ed8fbc42c7a87664fb93
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-12 16:26:52 -0300

    Dobro, Tiplo e raiz quadrada

---
+++ desafio006.py
@@ -0,0 +1,10 @@
+'''Crie um algoritimo que leia um numero e mostre o seu dobro, triplo e raiz quadrada '''
+
+n = int(input('Digite um numero:'))
+d = n * 2
+t = n * 3
+raiz = n **(1/2)
+print('Numero digitaddo foi {}.'.format(n))
+print('O Seu dobro é: {} '.format(d))
+print('O triplo é {}'.format(t))
+print('A raiz quadrada é {}.'.format(raiz))
