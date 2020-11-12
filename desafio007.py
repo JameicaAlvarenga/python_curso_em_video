848b64ce3df26ee9e4e90ceb2f0b5c64ab23a89e
JameicaAlvarenga <jnbrag@gmail.com>
2020-11-12 19:16:32 -0300

    formatando saída

--- desafio007.py
+++ desafio007.py
@@ -2,4 +2,4 @@
 
 n1 = float(input('Informe a primeira nota: '))
 n2 = float(input('Informe a segunda nota: '))
-print("A méida do aluno é :{}". format((n1+n2)/2))
+print("A méida do aluno é :{:.2f}". format((n1+n2)/2))
