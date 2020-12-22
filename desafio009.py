a72b1442cb0c53c1e11cecb6e9cd6ddf5127fb30
JameicaAlvarenga <jnbrag@gmail.com>
2020-12-22 15:58:10 -0300

    Incluindo funções para as operações

--- desafio009.py
+++ desafio009.py
@@ -4,21 +4,44 @@ print('1: Soma')
 print('2: Subtração')
 print('3: Multiplicação')
 print('4: Divisão')
+print('5: Todas')
 op = int(input('Escolha a operação:'))
 n = int(input("Digite um numero:"))
 a = 11
 
-if op == 1:
+def soma (n):
     for i in range(a):
-        print('{} + {} = {}'.format(n, i, n + i))
-elif op == 2:
+        print('{:2} + {:2} = {}'.format(n, i, n + i))
+
+def subtracao(n):
     for i in range(a):
-        print('{} - {} = {}'.format(n+i, n, (n+i) - n))
-elif op == 3:
-    for i in range(a):
-        print('{} * {} = {}'.format(n, i, n x i))
-elif op == 4:
+        print('{:2} - {:2} = {}'.format(n+i, n, (n+i) - n))
+
+def divisao(n):
     j = 0
     for i in range(1, a):
-        print('{} / {} = {}'.format((n+j), n, (n+j) / n))
+        print('{:2} / {:2} = {}'.format((n + j), n, (n + j) / n))
         j += n
+
+def multiplicacao(n):
+    for i in range(a):
+        print('{:2} * {:2} = {}'.format(n, i, (n * i)))
+
+
+if op == 1:
+    soma(n);
+
+elif op == 2:
+    subtracao(n);
+
+elif op == 3:
+    multiplicacao(n);
+
+elif op == 4:
+   divisao(n);
+
+elif op == 5:
+    soma(n)
+    subtracao(n)
+    multiplicacao(n)
+    divisao(n)
