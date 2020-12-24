0e066d8f89b75a80c633b16e68d0031082458b8b
JameicaAlvarenga <jnbrag@gmail.com>
2020-12-23 22:07:46 -0300

    Retornando o Valor do Dolar

--- desafio010.py
+++ desafio010.py
@@ -1,6 +1,14 @@
 '''Crie um programa que leia quanto de dinheiro uma pessoa tem na carteira e quanto dolares ela pode comprar'''
 
+import requests
+
 
 
 def conversor_dolar (n):
-    return (n * d);
+    url = f'https://economia.awesomeapi.com.br/all/{n}'
+    resp = requests.get(url)
+    return resp.json()['USD']['high']
+
+if __name__=='__main__':
+    print('Valor do dolar hoje é:',conversor_dolar('USD-BRL'))
+
