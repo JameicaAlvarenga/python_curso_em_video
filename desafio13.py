7448a11b2c35c576443978f2b1950698cafbf81a
JameicaAlvarenga <jnbrag@gmail.com>
2021-01-06 18:06:32 -0300

    '''Leia o salario de um funcionário e  mostre o seu novo salario com aumento de 15%'''

---
+++ desafio13.py
@@ -0,0 +1,6 @@
+'''Leia o salario de um funcionário e  mostre o seu novo salario com aumento de 15%'''
+
+salario = float(input('Informe o salário: R$'))
+aumento = float(input('Informe o percentual de aumento: '))
+reajuste= salario + (salario * aumento)/100
+print ('O salario passou de R$ {:.2f} para R$ {:.2f}'.format(salario,reajuste))
