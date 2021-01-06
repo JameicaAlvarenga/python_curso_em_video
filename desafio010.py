8e5990d925cce85a79169e8f01666ba5c303e5bd
JameicaAlvarenga <jnbrag@gmail.com>
2021-01-06 17:29:19 -0300

    desafio 10 usando API gratuita para buscar o valor do dolar.

--- desafio010.py
+++ desafio010.py
@@ -1,14 +1,18 @@
-'''Crie um programa que leia quanto de dinheiro uma pessoa tem na carteira e quanto dolares ela pode comprar'''
+'''Crie um programa que leia quanto de dinheiro uma pessoa tem na carteira e
+ quanto dolares ela pode comprar'''
 
 import requests
 
 
 
-def conversor_dolar (n):
-    url = f'https://economia.awesomeapi.com.br/all/{n}'
+def conversor_dolar (self):
+    url = f'https://economia.awesomeapi.com.br/all/{self}'
     resp = requests.get(url)
     return resp.json()['USD']['high']
 
 if __name__=='__main__':
     print('Valor do dolar hoje é:',conversor_dolar('USD-BRL'))
+    n=float(input('Valor de dinheiro em carteira em reais é:'))
+    print('Logo você pode comprar :{:.2f} dolares.'.format(n/float(conversor_dolar('USD-BRL'))))
+
 
