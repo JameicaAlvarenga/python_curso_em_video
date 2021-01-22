6a8030dc4fdff0885563081a01cdae7dfaa9d14e
JameicaAlvarenga <jnbrag@gmail.com>
2021-01-22 09:36:59 -0300

    leia uma frase pelo teclado e mostre quantas vezes aparece a letra A, em qual posição da primeira e ultima

---
+++ desafio26.py
@@ -0,0 +1,9 @@
+'''
+ Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”,
+ em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+'''
+
+texto = str(input('Digite o texto desejado: ')).strip()
+print('O texto digitado possui {} letras A.'.format(texto.upper().count('A',0,len(texto))))
+print('A primeira letra A está na posição numero : {}'.format(texto.upper().index('A')))
+print('A ultima  letra a está na posição numero : {}'.format(texto.upper().rfind('A')+1))
