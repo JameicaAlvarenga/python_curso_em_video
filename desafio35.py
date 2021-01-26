9502b8a3c8ad3c90ccd5d0d9590225ca6daf6f71
JameicaAlvarenga <jnbrag@gmail.com>
2021-01-26 19:02:54 -0300

    'leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.'

---
+++ desafio35.py
@@ -0,0 +1,13 @@
+'''
+Desenvolva um programa que leia o comprimento de três retas e diga ao usuário
+se elas podem ou não formar um triângulo.
+'''
+
+l1= float(input('Valor 1:'))
+l2= float(input('Valor 2:'))
+l3= float(input('Valor 3:'))
+
+if (l1+l2)>l3 and (l2+l3)>l1:
+    print('Esses valores podem formar um triangulo')
+else:
+    print('Esses valores não podem formar um triangulo')
