12d2f361c3bafecff08d9d1d274aa27ced5ed42d
JameicaAlvarenga <jnbrag@gmail.com>
2021-03-31 17:30:21 -0300

    conversão numero

--- desafio37.py
+++ desafio37.py
@@ -1,11 +1,11 @@
 '''Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário
 escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.'''
 
-n = int(input('Digite um numero: '))
-op = input('Escolha a opção: '
-           '1 - Binário '
-           '2 - Octadecimal '
-           '3 - Hexadecimal ')
+n = int(input('Digite um numero inteiro: '))
+op = input('''Escolha a opção para converter: 
+                1 - Binário 
+                2 - Octal 
+                3 - Hexadecimal ''')
 
 if op == '1':
     print(f'o numero, {n} convertido em Binário é: {bin(n)}')
