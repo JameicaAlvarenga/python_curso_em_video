d9c90457444a897a9fa0839f5e68507111efe4f5
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-21 14:37:41 -0300

    contagem regressiva

---
+++ desafio46.py
@@ -0,0 +1,9 @@
+'''46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
+indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
+import time
+for n in range(10,-1,-1):
+    print(n)
+    time.sleep(0.5)
+
+print('BUM!!BUM!!!')
+
