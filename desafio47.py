970c26f1fc333ebd1e4751282d3f5b451a6e4f9b
JameicaAlvarenga <jnbrag@gmail.com>
2021-07-21 15:16:30 -0300

    sequencia numerica par e impar usando for

--- desafio47.py
+++ desafio47.py
@@ -8,3 +8,16 @@ for n in range(1,51):
 
 for n in range(2,51,2):
     print(n)
+
+
+print('\nDesafio48: Faça um programa que calcule a soma entre todos os números impares que são múltiplos '
+      'de três e que se encontram no intervalo de 1 até 500.')
+soma=0
+qtd=0
+for n in range (1,501, 2):
+    if  n%3==0:
+        qtd+=1
+        print(n, end=" ")
+        soma+=n
+print('\nA soma entre esses valores é:{}'.format(soma))
+print(f'Foram somados:{qtd}')
