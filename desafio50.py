33aa79f5e98e54370f50f83d8527c9776ae4839c
JameicaAlvarenga <jnbrag@gmail.com>
2021-08-02 10:05:20 -0300

    Testando For e if com listas

--- desafio50.py
+++ desafio50.py
@@ -1,10 +1,12 @@
 '''Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que
  forem pares. Se o valor digitado for ímpar, desconsidere-o.'''
 lista = []
+cont=0
 for n in range(1, 7):
     numero = int(input("Dgigite um  numero inteiro: "))
+    cont +=1
     if numero % 2 == 0:
         lista.append(numero)
 
-print(lista)
-print('Soma dos valores é :', sum(lista))
+
+print('Voce informou {} números e a soma dos números pares informados é :{}'.format(cont,sum(lista)))
