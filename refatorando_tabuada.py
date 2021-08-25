e3a25a254ed407c7a71a03bd59fcaef6df932d98
JameicaAlvarenga <jnbrag@gmail.com>
2021-08-25 14:33:03 -0300

    Refatorando desafio009, incluido classe e metodos

--- refatorando_tabuada.py
+++ refatorando_tabuada.py
@@ -1,6 +1,7 @@
 "Tabuada "
 import self as self
-print('='*100,'\nGERADOR DE TABUADA\n','='*100)
+
+print('=' * 100, '\nGERADOR DE TABUADA\n', '=' * 100)
 print('1: Soma')
 print('2: Subtração')
 print('3: Multiplicação')
@@ -10,44 +11,43 @@ op = int(input('Escolha a operação:'))
 n = int(input("Escolha um numero para gerar a tabuada:"))
 a = 11
 
+
 class Tabuada:
-    def __init__(self,n):
-        self.num_tabuada=n
+    def __init__(self, n):
+        self.num_tabuada = n
 
-
-    def soma (self):
+    def soma(self):
         print('\nSoma:\n')
         for i in range(a):
             print('{:2} + {:2} = {}'.format(self.num_tabuada, i, self.num_tabuada + i))
 
-
     def subtracao(self):
         print('\nSubtração:\n')
         for i in range(a):
-             print('{:2} - {:2} = {}'.format(self.num_tabuada + i, self.num_tabuada, (self.num_tabuada + i) - self.num_tabuada))
-
+            print('{:2} - {:2} = {}'.format(self.num_tabuada + i, self.num_tabuada,
+                                            (self.num_tabuada + i) - self.num_tabuada))
 
     def divisao(self):
         print('\nDivisão::\n')
         j = 0
         for i in range(1, a):
-            print('{:2} / {:2} = {}'.format((self.num_tabuada + j), self.num_tabuada, (self.num_tabuada + j) / self.num_tabuada))
+            print('{:2} / {:2} = {}'.format((self.num_tabuada + j), self.num_tabuada,
+                                            (self.num_tabuada + j) / self.num_tabuada))
             j += self.num_tabuada
-
 
     def multiplicacao(self):
         print('\nMultiplicação:\n')
         for i in range(a):
-             print('{:2} * {:2} = {}'.format(self.num_tabuada, i, (self.num_tabuada * i)))
+            print('{:2} * {:2} = {}'.format(self.num_tabuada, i, (self.num_tabuada * i)))
 
 
-tabuada=Tabuada(n)
+tabuada = Tabuada(n)
 
 if op == 1:
-   tabuada.soma()
+    tabuada.soma()
 
 elif op == 2:
-   tabuada.subtracao()
+    tabuada.subtracao()
 
 elif op == 3:
     tabuada.multiplicacao()
